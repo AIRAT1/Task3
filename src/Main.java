2270d0f06e8136466f6ceb7dcc839e6389697e8f
AIRAT1 <ayrat1@mail.ru>
2019-09-19 16:41:15 +0300

    fixed

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,23 @@ public class Main {
     private static final int SILVER_LEVEL = 15000;
 
     public static void main(String[] args) {
-        int oldAmount = 200;
-        int currentAmount = 10000;
-        int newAmount = oldAmount + currentAmount;
-        int discountCoefficient = newAmount > GOLD_LEVEL ? 100 : newAmount > SILVER_LEVEL ? 70 : 50;
+        int accumulatedAmount = 150100;
+        int currentPurchase = 10000;
+        int bonusAmount = calculateBonus(accumulatedAmount); // we consider bonus amount according to the old amount value
+        int bonusValue = (currentPurchase / 1000) * bonusAmount; //and apply them to the last purchase
 
-        int discountValue = (currentAmount / 1000) * discountCoefficient;
-        System.out.println(discountValue);
+        // after accruing bonuses we increase the total amount of purchases by the current value... And save it
+        accumulatedAmount += currentPurchase;
+        System.out.println(bonusValue);
+    }
+
+    private static int calculateBonus(int amount) {
+        if (amount > GOLD_LEVEL) {
+            return 100;
+        }else if (amount > SILVER_LEVEL) {
+            return 70;
+        }else {
+            return 50;
+        }
     }
 }
