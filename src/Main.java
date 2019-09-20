1fb3f5a96331126e9db1dcbf20eafcc5732e6363
AIRAT1 <ayrat1@mail.ru>
2019-09-20 17:34:42 +0300

    comments deleted, javadoc added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
-
     private static final int GOLD_LEVEL = 150000;
     private static final int SILVER_LEVEL = 15000;
 
     public static void main(String[] args) {
-        int accumulatedAmount = 150100;
-        int currentPurchase = 10000;
-        int bonusAmount = calculateBonus(accumulatedAmount); // we consider bonus amount according to the old amount value
-        int bonusValue = (currentPurchase / 1000) * bonusAmount; //and apply them to the last purchase
+        calculateBonusValue();
+    }
 
-        // after accruing bonuses we increase the total amount of purchases by the current value... And save it
+    /**
+     * The method used for consider bonus amount according to the old amount value
+     * and apply them to the last purchase
+     * after accruing bonuses we increase the total amount of purchases by the current value and save it
+     */
+    private static void calculateBonusValue() {
+        int accumulatedAmount = 100100;
+        int currentPurchase = 10000;
+        int bonusAmount = calculateBonus(accumulatedAmount);
+        int bonusValue = (currentPurchase / 1000) * bonusAmount;
+
         accumulatedAmount += currentPurchase;
         System.out.println(bonusValue);
     }
